0e2865078e82220336768b3fe959bd98fff4c078
Spencer Wyckoff <spencer.wyckoff@gmail.com>
2015-02-25 20:40:09 -0500

    display routines w/ angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,56 @@
-console.log('The Iron Yard Rocks');
+;(function() {
+
+	window.app = angular.module('morningRoutine', []);
+
+	//Empty Routine Object
+	var addRoutine = {
+		name: '',
+		time: '',
+		description: '',
+		canAdd: false
+	};
+
+	//Array of Routines
+	var myRoutines = [	
+	{
+		name: 'Use the restroom',
+		time: 1,
+		description: '',
+		canAdd: true,
+	},
+
+	{
+		name: 'Calm.com meditation',
+		time: 10,
+		description: 'Immediately upon waking, take 10 minutes to practice mindfullness through a guided meditation.',
+		canAdd: true,
+	},
+
+	{
+		name: 'Lukewarm to cold shower',
+		time: 5,
+		description: 'Start off with the water being lukewarm, finish with the shower being only cold.  This has many physiological benefits and is better than a cup of coffee.',
+		canAdd: true,
+	},
+
+	];
+
+
+	//Add Routine Controller
+	app.controller('addRoutine', function() {
+
+		this.addRoutine = addRoutine;
+
+	});	
+
+	//Routine List Controller
+	app.controller('showRoutine', function() {
+
+		this.routines = myRoutines;
+
+	});
+
+	
+
+
+}());
